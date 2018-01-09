d5f93ecad79e8ad02443c1a76c06719eca189179
zaccharie <zaccharie.meddah@gmail.com>
2018-01-09 17:38:17 +0100

    exo 1 -> 6

--- src/test/java/java8/ex07/DateAndTime_07_Test.java
+++ src/test/java/java8/ex07/DateAndTime_07_Test.java
@@ -3,6 +3,7 @@ package java8.ex07;
 import org.junit.Test;
 
 import java.time.DayOfWeek;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.Date;
@@ -18,13 +19,13 @@ public class DateAndTime_07_Test {
     @Test
     public void test_date_to_localdate() throws Exception {
 
-        // TODO Créer une date Java 1 (12/02/2017)
-        Date date = null;
+        // Créer une date Java 1 (12/02/2017)
+        Date date = new Date(117, 1, 12);
 
-        // TODO transformer la date en Instant
+        // transformer la date en Instant
+        Instant instant = date.toInstant() ;
 
-        // TODO transformer la date en LocalDate
-
+        // transformer la date en LocalDate
         LocalDate result = null;
 
         // TODO valoriser les différentes variables afin de rendre le test passant
